b23a375eb98544c508cbbf8c05de2ea119854604
S Sathiya Dhev <sathiyadhevsisro@gmail.com>
2024-05-22 14:59:53 +0530

    add .gitignore, C++/melon.cpp

---
+++ C++/melon.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int weight;
+    cin >> weight;
+    if(weight%2 == 0 and weight!=2) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
